5311c1c670731122e6d58303130df12ce1ae07d3
panopticons <thepanopticons@gmail.com>
2017-03-20 23:50:13 -0700

    updated

---
+++ pseudogram/postViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  postViewCell.swift
+//  pseudogram
+//
+//  Created by fer on 3/20/17.
+//  Copyright © 2017 fer. All rights reserved.
+//
+
+import UIKit
+
+class postViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
